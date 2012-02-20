832e409b2e75ee3302e0006f9b50c1222b296e09
mlctrez <mlctrez@gmail.com>
2012-02-19 18:18:35 -0600

    use spaces for formatting to improve readability
    Signed-off-by: mlctrez <mlctrez@gmail.com>

--- arduino_code.c
+++ arduino_code.c
@@ -14,114 +14,114 @@ uint8_t clockBit;
 uint8_t dataBit;
 uint8_t displayBit;
 
+// 320 bytes storage for the display
 uint8_t data[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void setup() // run once, when the sketch starts
 {
-	Serial.begin(9600);
+  Serial.begin(9600);
 
-	serInStr[0] = 0;
-	serInStr[1] = 0;
-	serInStr[2] = 0;
-	serInStr[3] = 0;
+  serInStr[0] = 0;
+  serInStr[1] = 0;
+  serInStr[2] = 0;
+  serInStr[3] = 0;
 
-	pinMode(2, OUTPUT);
-	digitalWrite(2, LOW);
-	pinMode(3, OUTPUT);
-	digitalWrite(3, LOW);
-	pinMode(4, OUTPUT);
-	digitalWrite(4, HIGH);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  pinMode(3, OUTPUT);
+  digitalWrite(3, LOW);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
 
-	out = portOutputRegister(digitalPinToPort(2));
-	clockBit = digitalPinToBitMask(2);
-	dataBit = digitalPinToBitMask(3);
-	displayBit = digitalPinToBitMask(4);
+  out = portOutputRegister(digitalPinToPort(2));
+  clockBit = digitalPinToBitMask(2);
+  dataBit = digitalPinToBitMask(3);
+  displayBit = digitalPinToBitMask(4);
 
 }
 
 void loop() {
 
-	for (int j = 1; j < 8; j++) {
+  for (int j = 1; j < 8; j++) {
 
-		*out |= displayBit;
+    *out |= displayBit;
 
-		if (readCommand(serInStr) > 0) {
-			if (serInStr[0] == CMNDR_START_BYTE) {
-				int address = serInStr[1] * 256 + serInStr[2];
-				int length = serInStr[3];
-				if (address > -1 && address < 320) {
-					for (int k = 0; k < length; k++) {
-						data[address + k] = serInStr[4 + k];
-					}
-				}
+    if (readCommand(serInStr) > 0) {
+      if (serInStr[0] == CMNDR_START_BYTE) {
+        int address = serInStr[1] * 256 + serInStr[2];
+        int length = serInStr[3];
+        if (address > -1 && address < 320) {
+          for (int k = 0; k < length; k++) {
+            data[address + k] = serInStr[4 + k];
+          }
+        }
 
-				if (address == 321)
-					brightness = serInStr[4];
-				if (address == 322)
-					onOff = serInStr[4];
-			}
-		}
+        if (address == 321)
+          brightness = serInStr[4];
+        if (address == 322)
+          onOff = serInStr[4];
+      }
+    }
 
-		if (1 == onOff) {
+    if (1 == onOff) {
 
-			// write memory to serial
-			for (int i = 0; i < 320; i++) {
-				shiftBit(!(data[i] & (1 << (j - 1))));
-			}
+      // write memory to serial
+      for (int i = 0; i < 320; i++) {
+        shiftBit(!(data[i] & (1 << (j - 1))));
+      }
 
-			// write appropriate row bit
-			shiftBit(j & 0x04);
-			shiftBit(j & 0x02);
-			shiftBit(j & 0x01);
+      // write appropriate row bit
+      shiftBit(j & 0x04);
+      shiftBit(j & 0x02);
+      shiftBit(j & 0x01);
 
-			// start displaying
-			*out &= ~displayBit;
-			delayMicroseconds(brightness);
-		}
-	}
+      // start displaying
+      *out &= ~displayBit;
+      delayMicroseconds(brightness);
+    }
+  }
 
 }
 
 void shiftBit(int val) {
-	if (val == LOW)
-		*out &= ~dataBit;
-	else
-		*out |= dataBit;
+  if (val == LOW)
+    *out &= ~dataBit;
+  else
+    *out |= dataBit;
 
-	*out |= clockBit;
-	*out &= ~clockBit;
+  *out |= clockBit;
+  *out &= ~clockBit;
 }
 
 uint8_t readCommand(byte *str) {
-	if (!Serial.available())
-		return 0;
+  if (!Serial.available())
+    return 0;
 
-	delay(20); // wait a little for serial data
+  delay(20); // wait a little for serial data
 
-	int c = Serial.read(); // get start byte (hopefully)
-	if (c != CMNDR_START_BYTE)
-		return 0;
-	str[0] = c;
+  int c = Serial.read(); // get start byte (hopefully)
+  if (c != CMNDR_START_BYTE)
+    return 0;
+  str[0] = c;
 
-	int i = 1;
-	while (Serial.available()) {
-		str[i] = Serial.read();
-		i++;
-		delay(1);
-	}
-	str[i] = 0;
-	return i;
+  int i = 1;
+  while (Serial.available()) {
+    str[i] = Serial.read();
+    i++;
+    delay(1);
+  }
+  str[i] = 0;
+  return i;
 }
